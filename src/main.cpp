5f148e344e079dfe13f81183d671e7eac5f277a9
bsker1 <kiserb17@gmail.com>
2025-12-08 15:23:38 -0500

    Initialize GLFW window and load GLAD for OpenGL calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,59 @@
+#include "glad/glad.h"
+#include "glfw/glfw3.h"
+
 #include <iostream>
 
+#define RESOLUTION_X 800
+#define RESOLUTION_Y 800
+
 int main() {
-  std::cout << "Hello world" << std::endl;
+  // Create GLFW windo object
+  GLFWwindow* window;
+
+  // Initialize GLFW
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  // Configure GLFW for OpenGL 3.3 core
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Create a windowed mode window and its OpenGL context
+  window = glfwCreateWindow(RESOLUTION_X, RESOLUTION_Y, "OpenGL Tutorial", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  // Make the window's context current
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLAD 
+  gladLoadGL();
+
+  // Define OpenGl viewport to size of window
+  glViewport(0, 0, RESOLUTION_X, RESOLUTION_Y);
+
+  while (!glfwWindowShouldClose(window)) {
+    // Clear window to black
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // CODE HERE
+
+
+    // Swap front and back buffers
+    glfwSwapBuffers(window);
+
+    // Poll for and process events
+    glfwPollEvents();
+  }
+
+  // Cleanup GLFW window object
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   return 0;
 }
